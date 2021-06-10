e4f762dbd2efbfdc6aaecda466cee857ff644f87
StefanaTM <stefana.toma@gmail.com>
2021-06-10 23:46:14 +0300

    activity number of groups of cards

---
+++ app/src/main/java/com/game/mixed/SetNumberCards.java
@@ -0,0 +1,14 @@
+package com.game.mixed;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SetNumberCards extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_number_cards);
+    }
+}
